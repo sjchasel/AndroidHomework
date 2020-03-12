a4f450b26b4ddfca225de8f514aeaec437abfc29
sjchasel <1296841437@qq.com>
2020-03-12 22:10:13 +0800

    Initial commit

---
+++ app/src/main/java/com/swufe/happybirthday/MainActivity.java
@@ -0,0 +1,15 @@
+package com.swufe.happybirthday;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
